09c9dada81648ab1932f5e68a7ff5e9ac8b74460
yeonise <cerealnum@gmail.com>
2024-06-08 15:51:42 +0900

    feat: [domain] User.java 생성

---
+++ src/main/java/me/yeon/thread/domain/User.java
@@ -0,0 +1,11 @@
+package me.yeon.thread.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class User {
+
+	private final int sessionCode;
+	private final String nickname;
+
+}

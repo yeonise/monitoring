7d197e922fd0a3d6714acbf8d5c6d53ffc0536ae
yeonise <cerealnum@gmail.com>
2024-06-08 15:53:46 +0900

    feat: [util] AppConstants.java 생성

---
+++ src/main/java/me/yeon/thread/util/AppConstants.java
@@ -0,0 +1,11 @@
+package me.yeon.thread.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class AppConstants {
+
+	public static final int DEFAULT_ROOM_CODE = 1227;
+
+}

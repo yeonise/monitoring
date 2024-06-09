9e808afb84eda1b3a675ba92adbe95ed4492c445
yeonise <cerealnum@gmail.com>
2024-06-09 17:34:03 +0900

    feat: update AppConstants.java

--- src/main/java/me/yeon/thread/util/AppConstants.java
+++ src/main/java/me/yeon/thread/util/AppConstants.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class AppConstants {
 
-	public static final int DEFAULT_ROOM_CODE = 1227;
+	public static final String SESSION_ATTRIBUTE_KEY = "session";
+	public static final String DEFAULT_ROOM_CODE = "1227";
+	public static final String DEFAULT_CHAT_LIST_KEY = "chat";
+	public static final String DEFAULT_PARTICIPANT_SET_KEY = "participant";
 
 }

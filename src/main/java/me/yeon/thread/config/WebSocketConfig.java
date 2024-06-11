3dade7c967c2328aad104b57a5a493d527acd039
yeonise <cerealnum@gmail.com>
2024-06-11 21:26:56 +0900

    remove: [util] StompChanelInterceptor 제거

--- src/main/java/me/yeon/thread/config/WebSocketConfig.java
+++ src/main/java/me/yeon/thread/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package me.yeon.thread.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,7 +8,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 import lombok.RequiredArgsConstructor;
 import me.yeon.thread.util.interceptor.SocketHandshakeInterceptor;
-import me.yeon.thread.util.interceptor.StompChanelInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -17,7 +15,6 @@ import me.yeon.thread.util.interceptor.StompChanelInterceptor;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	private final SocketHandshakeInterceptor socketHandshakeInterceptor;
-	private final StompChanelInterceptor stompChanelInterceptor;
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -31,11 +28,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.enableSimpleBroker("/sub");
 		registry.setApplicationDestinationPrefixes("/pub");
-	}
-
-	@Override
-	public void configureClientInboundChannel(ChannelRegistration registration) {
-		registration.interceptors(stompChanelInterceptor);
 	}
 
 }
